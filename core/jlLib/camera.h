031fd9795bb7610427f46ae15bde7a8dccc77668
Jayden Lombardi <jayden.lombardi@mymail.champlain.edu>
2023-10-17 11:07:59 -0400

    Finished the ortho and perspective fields for the camera.h Moved onto camera movement and controls made it down to Keyboard Movement section of the assignment

--- core/jlLib/camera.h
+++ core/jlLib/camera.h
@@ -2,6 +2,7 @@
 #include "../ew/ewMath/mat4.h"
 #include "../ew/ewMath/vec3.h"
 #include "../ew/ewMath/transformations.h"
+#include "../ew/ewMath/ewMath.h"
 //#include "transformations.h" 
 
 namespace myLib
@@ -30,9 +31,20 @@ namespace myLib
 			}
 			else {
 				//Perspective projection
-				float fovRadians = fov * 3.14159265358979323846f / 180.0f;
+				float fovRadians = ew::Radians(fov);
 				return ew::Perspective(fovRadians, aspectRatio, nearPlane, farPlane);  
 			}
 		}
+	};
+
+	struct CameraControls
+	{
+		double prevMouseX, prevMouseY; //Mouse Pos from previous frame
+		
+		float yaw = 0, pitch = 0; //degrees
+		float mousesSens = 0.1f; //how fast to turn with mouse
+		float moveSpeed = 5.0f; //how fast to move with arrow keys
+		bool firstMouse = true; //flag to score intitial mouse position
+
 	};
 }
