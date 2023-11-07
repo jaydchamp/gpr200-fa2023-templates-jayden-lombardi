3a1a13c90e447f62e6fa102fefdd0e8afa4f12f0
Jayden Lombardi <jayden.lombardi@mymail.champlain.edu>
2023-11-07 17:42:49 -0500

    Attempted to fix, did not work

--- assignments/assignment6_proceduralGeometry/main.cpp
+++ assignments/assignment6_proceduralGeometry/main.cpp
@@ -61,21 +61,6 @@ int main() {
 		return 1;
 	}
 
-	//ew::MeshData sphereMeshData = myLib::createSphere(0.5f, 64); 
-	//ew::MeshData cylinderMeshData = myLib::createSphere(0.5f, 64); 
-	//ew::MeshData planeMeshData = myLib::createSphere(0.5f, 64); 
-	//create mesh render
-	//ew::Mesh sphereMesh(sphereMeshData); 
-	//ew::Mesh cylinderMesh(cylinderMeshData); 
-	//ew::Mesh planeMesh(planeMeshData); 
-	//initialize transform
-	//ew::Transform sphereTransform; 
-	//ew::Transform cylinderTransform; 
-	//ew::Transform planeTransform; 
-	//sphereTransform.position = ew::Vec3(1.0f, 0.0f, 0.0f); 
-	//cylinderTransform.position = ew::Vec3(1.0f, 0.0f, 0.0f); 
-	//planeTransform.position = ew::Vec3(1.0f, 0.0f, 0.0f);
-
 	//Initialize ImGUI
 	IMGUI_CHECKVERSION();
 	ImGui::CreateContext();
@@ -94,19 +79,19 @@ int main() {
 	ew::Shader shader("assets/vertexShader.vert", "assets/fragmentShader.frag");
 	unsigned int brickTexture = ew::loadTexture("assets/brick_color.jpg",GL_REPEAT,GL_LINEAR);
 
-	ew::Mesh cubeMesh(ew::createCube(1.0f));
-	ew::Mesh planeMesh(myLib::createPlane(5.0f, 5.0f, 10));
+	//ew::Mesh cubeMesh(ew::createCube(1.0f));
+	//ew::Mesh planeMesh(myLib::createPlane(5.0f, 5.0f, 10));
 	ew::Mesh sphereMesh(myLib::createSphere(0.5f, 64));
-	ew::Mesh cylinderMesh(myLib::createCylinder(0.5f, 1.0f, 32));
+	//ew::Mesh cylinderMesh(myLib::createCylinder(0.5f, 1.0f, 32));
 
 	//Initialize transforms
-	ew::Transform cubeTransform;
-	ew::Transform planeTransform;
+	//ew::Transform cubeTransform;
+	//ew::Transform planeTransform;
 	ew::Transform sphereTransform;
-	ew::Transform cylinderTransform;
-	planeTransform.position = ew::Vec3(0, -1.0, 0);
+	//ew::Transform cylinderTransform;
+	//planeTransform.position = ew::Vec3(0, -1.0, 0);
 	sphereTransform.position = ew::Vec3(-1.5f, 0.0f, 0.0f);
-	cylinderTransform.position = ew::Vec3(1.5f, 0.0f, 0.0f);
+	//cylinderTransform.position = ew::Vec3(1.5f, 0.0f, 0.0f);
 
 	resetCamera(camera,cameraController);
 
@@ -137,14 +122,14 @@ int main() {
 		shader.setVec3("_LightDir", lightF);
 
 		//draw shapes
-		shader.setMat4("_Model", cubeTransform.getModelMatrix());
-		cubeMesh.draw();
-		shader.setMat4("_Model", planeTransform.getModelMatrix());
-		planeMesh.draw();
+		//shader.setMat4("_Model", cubeTransform.getModelMatrix());
+		//cubeMesh.draw();
+		//shader.setMat4("_Model", planeTransform.getModelMatrix());
+		//planeMesh.draw();
 		shader.setMat4("_Model", sphereTransform.getModelMatrix());
 		sphereMesh.draw();
-		shader.setMat4("_Model", cylinderTransform.getModelMatrix());
-		cylinderMesh.draw();
+		//shader.setMat4("_Model", cylinderTransform.getModelMatrix());
+		//cylinderMesh.draw();
 
 		//sphereMesh.draw((ew::DrawMode)appSettings.drawAsPoints); 
 
--- core/jlLib/procGen.cpp
+++ core/jlLib/procGen.cpp
@@ -1,156 +1,225 @@
 #include "procGen.h"
-#define PI 3.14159265359 
+//#define PI 3.14159265359 
 
 ew::MeshData myLib::createSphere(float radius, int numSegments)
 {
-	ew::MeshData sphereMeshData;
+	//Code Taken from assignment 7:
+	//Still giving the same errors 
+	//thought it was a problem in my functions but I changed and tested them
 
-	for (int i = 0; i <= numSegments; i++)
+	ew::MeshData mesh;
+	//VERTICES
+	float thetaStep = ew::TAU / numSegments;
+	float phiStep = ew::PI / numSegments;
+	for (size_t row = 0; row <= numSegments; row++)
 	{
-		float v = static_cast<float>(i) / static_cast<float>(numSegments);
-
-		float polarAngle = v * PI; // angle from 'Pole' 0 to PI
-		float sinPolar = sin(polarAngle);
-		float cosPolar = cos(polarAngle);
-
-		for (int j = 0; j <= numSegments; j++)
+		float phi = row * phiStep;
+		for (size_t col = 0; col <= numSegments; col++)
 		{
-			float u = static_cast<float>(j) / static_cast<float>(numSegments);
-
-			float azimuthAngle = u * 2 * PI; // Angle around the sphere (0 to 2pi)
-			float sinAzimuth = sin(azimuthAngle);
-			float cosAzimuth = cos(azimuthAngle);
-
-			ew::Vertex vertex;
-			vertex.pos.x = radius * cosAzimuth * sinPolar; // x = radius * cos(a) * sin(p) 
-			vertex.pos.y = radius * cosPolar; // y = r * cos(p) 
-			vertex.pos.z = radius * sinAzimuth * sinPolar; // z = r * sin(a) * sin(p)
-
-			//normalize vector
-			float length = sqrt(vertex.pos.x * vertex.pos.x + vertex.pos.y * vertex.pos.y + vertex.pos.z * vertex.pos.z);
-			if (length != 0)
-			{
-				vertex.normal.x = vertex.pos.x / length;
-				vertex.normal.y = vertex.pos.y / length;
-				vertex.normal.z = vertex.pos.z / length;
-			}
-			else
-			{
-				vertex.normal = ew::Vec3(0, 1, 0); // setting default normal if length is 0
-			}
-			sphereMeshData.vertices.push_back(vertex);
+			float theta = thetaStep * col;
+			ew::Vertex v;
+			v.normal.x = cosf(theta) * sinf(phi);
+			v.normal.y = cosf(phi);
+			v.normal.z = sinf(theta) * sinf(phi);
+			v.pos = v.normal * radius;
+			v.uv.x = (float)col / numSegments;
+			v.uv.y = 1.0 - ((float)row / numSegments);
+			mesh.vertices.push_back(v);
 		}
 	}
 
-	for (int i = 0; i < numSegments; ++i) 
+	//INDICES
+	unsigned int columns = numSegments + 1;
+	unsigned int sideStart = columns;
+	unsigned int poleStart = 0;
+	//Top cap
+	for (size_t i = 0; i < numSegments; i++)
 	{
-		for (int j = 0; j < numSegments; ++j) 
+		mesh.indices.push_back(sideStart + i);
+		mesh.indices.push_back(poleStart + i);
+		mesh.indices.push_back(sideStart + i + 1);
+	}
+	//Rows of quads for sides
+	for (size_t row = 1; row < numSegments - 1; row++)
+	{
+		for (size_t col = 0; col < numSegments; col++)
 		{
-			int topLeft = (i * (numSegments + 1)) + j;
-			int topRight = topLeft + 1;
-			int bottomLeft = topLeft + numSegments + 1;
-			int bottomRight = bottomLeft + 1;
-
-			sphereMeshData.indices.push_back(topLeft);
-			sphereMeshData.indices.push_back(bottomLeft);
-			sphereMeshData.indices.push_back(topRight);
-
-			sphereMeshData.indices.push_back(topRight);
-			sphereMeshData.indices.push_back(bottomLeft);
-			sphereMeshData.indices.push_back(bottomRight);
+			int start = row * columns + col;
+			mesh.indices.push_back(start);
+			mesh.indices.push_back(start + 1);
+			mesh.indices.push_back(start + columns);
+			mesh.indices.push_back(start + columns);
+			mesh.indices.push_back(start + 1);
+			mesh.indices.push_back(start + columns + 1);
 		}
 	}
+	//Bottom cap
+	poleStart = (columns * columns) - columns;
+	sideStart = poleStart - columns;
+	for (size_t i = 0; i < numSegments; i++)
+	{
+		mesh.indices.push_back(sideStart + i);
+		mesh.indices.push_back(sideStart + i + 1);
+		mesh.indices.push_back(poleStart + i);
+	}
+	return mesh;
 
-	return sphereMeshData;
+	//My Original Code::
+
+	//ew::MeshData sphereMeshData;
+
+	//for (int i = 0; i <= numSegments; i++)
+	//{
+	//	float v = static_cast<float>(i) / static_cast<float>(numSegments);
+
+	//	float polarAngle = v * PI; // angle from 'Pole' 0 to PI
+	//	float sinPolar = sin(polarAngle);
+	//	float cosPolar = cos(polarAngle);
+
+	//	for (int j = 0; j <= numSegments; j++)
+	//	{
+	//		float u = static_cast<float>(j) / static_cast<float>(numSegments);
+
+	//		float azimuthAngle = u * 2 * PI; // Angle around the sphere (0 to 2pi)
+	//		float sinAzimuth = sin(azimuthAngle);
+	//		float cosAzimuth = cos(azimuthAngle);
+
+	//		ew::Vertex vertex;
+	//		vertex.pos.x = radius * cosAzimuth * sinPolar; // x = radius * cos(a) * sin(p) 
+	//		vertex.pos.y = radius * cosPolar; // y = r * cos(p) 
+	//		vertex.pos.z = radius * sinAzimuth * sinPolar; // z = r * sin(a) * sin(p)
+
+	//		//normalize vector
+	//		float length = sqrt(vertex.pos.x * vertex.pos.x + vertex.pos.y * vertex.pos.y + vertex.pos.z * vertex.pos.z);
+	//		if (length != 0)
+	//		{
+	//			vertex.normal.x = vertex.pos.x / length;
+	//			vertex.normal.y = vertex.pos.y / length;
+	//			vertex.normal.z = vertex.pos.z / length;
+	//		}
+	//		else
+	//		{
+	//			vertex.normal = ew::Vec3(0, 1, 0); // setting default normal if length is 0
+	//		}
+	//		sphereMeshData.vertices.push_back(vertex);
+	//	}
+	//}
+
+	//for (int i = 0; i < numSegments; ++i) 
+	//{
+	//	for (int j = 0; j < numSegments; ++j) 
+	//	{
+	//		int topLeft = (i * (numSegments + 1)) + j;
+	//		int topRight = topLeft + 1;
+	//		int bottomLeft = topLeft + numSegments + 1;
+	//		int bottomRight = bottomLeft + 1;
+
+	//		sphereMeshData.indices.push_back(topLeft);
+	//		sphereMeshData.indices.push_back(bottomLeft);
+	//		sphereMeshData.indices.push_back(topRight);
+
+	//		sphereMeshData.indices.push_back(topRight);
+	//		sphereMeshData.indices.push_back(bottomLeft);
+	//		sphereMeshData.indices.push_back(bottomRight);
+	//	}
+	//}
+
+	//return sphereMeshData;
 }
 
 ew::MeshData myLib::createCylinder(float height, float radius, int numSegments)
 {
-	ew::MeshData cylinderMeshData;
 
-	float segmentHeight = height / static_cast<float>(numSegments);
-	float segmentAngle = (2 * PI) / static_cast<float>(numSegments);
+	//My Original Code::
 
-	for (int i = 0; i < numSegments; ++i) 
-	{
-		float polarAngle = segmentAngle * i;
-		float x = radius * cos(polarAngle);
-		float z = radius * sin(polarAngle);
+	//ew::MeshData cylinderMeshData;
 
-		for (int j = 0; j < 2; ++j) //twice for top and bottom circles
-		{
-			float y; 
-			if (j == 0) 
-			{
-				y = -height / 2.0f;
-			}
-			else 
-			{
-				y = height / 2.0f;
-			}
+	//float segmentHeight = height / static_cast<float>(numSegments);
+	//float segmentAngle = (2 * PI) / static_cast<float>(numSegments);
 
-			ew::Vertex vertex;
-			vertex.pos = { x, y, z };
-			vertex.normal = { x, 0.0f, z }; // Normal is same as position for a simple example
-			vertex.uv = { static_cast<float>(i) / numSegments, static_cast<float>(j) }; // Adjust UVs as needed
+	//for (int i = 0; i < numSegments; ++i) 
+	//{
+	//	float polarAngle = segmentAngle * i;
+	//	float x = radius * cos(polarAngle);
+	//	float z = radius * sin(polarAngle);
 
-			cylinderMeshData.vertices.push_back(vertex);
-		}
-	}
+	//	for (int j = 0; j < 2; ++j) //twice for top and bottom circles
+	//	{
+	//		float y; 
+	//		if (j == 0) 
+	//		{
+	//			y = -height / 2.0f;
+	//		}
+	//		else 
+	//		{
+	//			y = height / 2.0f;
+	//		}
 
-	for (int i = 0; i < numSegments; ++i) 
-	{
-		int next = (i + 1) % numSegments;
-		cylinderMeshData.indices.push_back(i * 2);
-		cylinderMeshData.indices.push_back(next * 2);
-		cylinderMeshData.indices.push_back(next * 2 + 1);
+	//		ew::Vertex vertex;
+	//		vertex.pos = { x, y, z };
+	//		vertex.normal = { x, 0.0f, z }; // Normal is same as position for a simple example
+	//		vertex.uv = { static_cast<float>(i) / numSegments, static_cast<float>(j) }; // Adjust UVs as needed
 
-		cylinderMeshData.indices.push_back(i * 2);
-		cylinderMeshData.indices.push_back(next * 2 + 1);
-		cylinderMeshData.indices.push_back(i * 2 + 1);
-	}
+	//		cylinderMeshData.vertices.push_back(vertex);
+	//	}
+	//}
 
-	return cylinderMeshData;
+	//for (int i = 0; i < numSegments; ++i) 
+	//{
+	//	int next = (i + 1) % numSegments;
+	//	cylinderMeshData.indices.push_back(i * 2);
+	//	cylinderMeshData.indices.push_back(next * 2);
+	//	cylinderMeshData.indices.push_back(next * 2 + 1);
+
+	//	cylinderMeshData.indices.push_back(i * 2);
+	//	cylinderMeshData.indices.push_back(next * 2 + 1);
+	//	cylinderMeshData.indices.push_back(i * 2 + 1);
+	//}
+
+	//return cylinderMeshData;
 }
 
 ew::MeshData myLib::createPlane(float width, float height, int subDivisions)
 {
-	ew::MeshData planeMeshData;
 
-	float stepX = width / subDivisions;
-	float stepY = height / subDivisions;
+	//My Original Code::
 
-	for (int i = 0; i <= subDivisions; ++i) 
-	{
-		for (int j = 0; j <= subDivisions; ++j) 
-		{
-			ew::Vertex vertex;
-			vertex.pos = { j * stepX - (width / 2.0f), 0.0f, i * stepY - (height / 2.0f) }; 
-			vertex.normal = { 0.0f, 1.0f, 0.0f };
-			vertex.uv = { static_cast<float>(j) / subDivisions, static_cast<float>(i) / subDivisions }; 
+	//ew::MeshData planeMeshData;
 
-			planeMeshData.vertices.push_back(vertex);
-		}
-	}
+	//float stepX = width / subDivisions;
+	//float stepY = height / subDivisions;
 
-	for (int i = 0; i < subDivisions; ++i) 
-	{
-		for (int j = 0; j < subDivisions; ++j) 
-		{
-			int topLeft = i * (subDivisions + 1) + j; 
-			int topRight = topLeft + 1;  
-			int bottomLeft = (i + 1) * (subDivisions + 1) + j; 
-			int bottomRight = bottomLeft + 1; 
+	//for (int i = 0; i <= subDivisions; ++i) 
+	//{
+	//	for (int j = 0; j <= subDivisions; ++j) 
+	//	{
+	//		ew::Vertex vertex;
+	//		vertex.pos = { j * stepX - (width / 2.0f), 0.0f, i * stepY - (height / 2.0f) }; 
+	//		vertex.normal = { 0.0f, 1.0f, 0.0f };
+	//		vertex.uv = { static_cast<float>(j) / subDivisions, static_cast<float>(i) / subDivisions }; 
 
-			planeMeshData.indices.push_back(topLeft); 
-			planeMeshData.indices.push_back(bottomLeft); 
-			planeMeshData.indices.push_back(topRight); 
+	//		planeMeshData.vertices.push_back(vertex);
+	//	}
+	//}
 
-			planeMeshData.indices.push_back(topRight); 
-			planeMeshData.indices.push_back(bottomLeft); 
-			planeMeshData.indices.push_back(bottomRight); 
-		}
-	}
+	//for (int i = 0; i < subDivisions; ++i) 
+	//{
+	//	for (int j = 0; j < subDivisions; ++j) 
+	//	{
+	//		int topLeft = i * (subDivisions + 1) + j; 
+	//		int topRight = topLeft + 1;  
+	//		int bottomLeft = (i + 1) * (subDivisions + 1) + j; 
+	//		int bottomRight = bottomLeft + 1; 
 
-	return planeMeshData;
+	//		planeMeshData.indices.push_back(topLeft); 
+	//		planeMeshData.indices.push_back(bottomLeft); 
+	//		planeMeshData.indices.push_back(topRight); 
+
+	//		planeMeshData.indices.push_back(topRight); 
+	//		planeMeshData.indices.push_back(bottomLeft); 
+	//		planeMeshData.indices.push_back(bottomRight); 
+	//	}
+	//}
+
+	//return planeMeshData;
 }
